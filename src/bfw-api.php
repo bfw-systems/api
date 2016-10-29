8727944b4c1c63ba0595100510170c25625eb4e3
bulton-fr <bulton.fr@gmail.com>
2016-10-29 21:36:30 +0200

    Remove not used and move v1.0.x files

--- src/bfw-api.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Actions à effectuer lors de l'initialisation du module par le framework.
- * @author Vermeulen Maxime <bulton.fr@gmail.com>
- * @package bfw-api
- * @version 1.0
- */
-
-$Modules->newMod('bfw-api', array('time' => modulesLoadTime_EndInit));
