baa7549d860ce4749016671c249826237fec1cb8
bulton-fr <bulton.fr@gmail.com>
2017-03-20 22:44:10 +0100

    Run test : Fix config file

--- test/run/skeleton/app/config/bfw-api/config.php
+++ test/run/skeleton/app/config/bfw-api/config.php
@@ -11,5 +11,19 @@ return (object) [
      * @var string $urlPrefix : Prefix url to use for all api url
      * @example '/api' for have an api url /api/books/
      */
-    'urlPrefix' => '/api'
+    'urlPrefix' => '/api',
+    
+    /**
+     * @var boolean $useRest : To use REST api mode
+     */
+    'useRest' => true,
+    
+    /**
+     * @var boolean $useGraphQL : To use GraphQL api mode (WIP)
+     * @link https://github.com/bulton-fr/bfw-api/issues/2
+     * 
+     * Not implemented yet. If you set the value to true, you will obtain a
+     * 501 http response.
+     */
+    'useGraphQL' => false
 ];
