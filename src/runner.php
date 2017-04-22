43140217d3c760a4761194d0bd5c3309387504e2
bulton-fr <bulton.fr@gmail.com>
2017-04-22 19:47:37 +0200

    Add \Api namespace to composer

--- src/runner.php
+++ src/runner.php
@@ -4,3 +4,6 @@ $bfwApi = new \BfwApi\BfwApi($module);
 
 $app = \BFW\Application::getInstance();
 $app->attach($bfwApi);
+
+\BFW\Helpers\Constants::create('API_DIR', SRC_DIR.'api/');
+$app->getComposerLoader()->addPsr4('Api\\', API_DIR);
